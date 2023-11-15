5dc209409d85f3b97c960b52194bc9ad9d967064
Jayse Benson <jaysebenson@hotmail.com>
2023-11-14 19:10:56 -0500

    completed assignment

---
+++ src/homework/variable/errsAsgn.java
@@ -0,0 +1,31 @@
+package homework.variable;
+
+public class errsAsgn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		//String thing = 8;
+		// type mismatch
+		
+		int a= 2;
+		int b = 2;
+		
+		
+		//let cat = 7
+		//syntax error
+		
+		
+		int[] arr = new int[8];
+		
+		//System.out.println(arr[8]);
+		// out of bounds exception
+		
+		
+		
+
+	}
+
+}
